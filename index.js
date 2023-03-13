dd14ea44fead5d9208a3b3410d592d2da39c2bb7
Robx33 <robsonc25@outlook.com>
2023-03-13 15:50:02 -0300

    comments

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const navBar = document.querySelector(".navbar")
 const allLi = document.querySelectorAll('li')
 
-allLi.forEach((li, index) => {
+allLi.forEach((li, index) => { // forEach percorre minhas li e com evento de click remove a classe especificada e adciona uma outra
 
     li.addEventListener('click', e => {
-        navBar.querySelector(".active-list").classList.remove("active-list")
+        navBar.querySelector(".active-list").classList.remove("active-list") 
         li.classList.add("active-list")
 
         const indicator = document.querySelector(".indicator")
-        indicator.style.transform = `translateX(calc(${index * 90}px))`
+        indicator.style.transform = `translateX(calc(${index * 90}px))` // Colocando o estilo 'transform'
     })
 })
+
